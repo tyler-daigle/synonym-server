363ca319ad976d010cd240c797400687c5ff196d
Tyler D <tyler@tdaigle.com>
2021-07-18 16:32:59 -0400

    added process.env.PORT for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const API_KEY = process.env.API_KEY;
 
 if (typeof API_KEY === "undefined") {
