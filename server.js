4efae449a5e3e3d6c382a66f45b10e672f88dfc2
Tyler D <tyler@tdaigle.com>
2021-07-18 16:05:16 -0400

    synonym server successful

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+const port = 8000;
+const API_KEY = process.env.API_KEY;
+
+if (typeof API_KEY === "undefined") {
+  console.error(
+    "You need to set the api key for the thesauarus in the API_KEY environment variable."
+  );
+  process.exit();
+}
+
+app.get("/api", async (req, res) => {
+  console.log(req.query.word);
+  const word = req.query.word;
+  const apiUrl = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${API_KEY}`;
+  const data = await fetch(apiUrl);
+  const json = await data.json();
+
+  const wordList = json[0].meta.syns[0];
+  res.json(wordList);
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
